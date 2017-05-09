3a43fcf3666deb9673b7baf083b8af016340fd7e
meckert-salesforce <meckert@salesforce.com>
2017-05-09 08:57:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-    response.send('Hello Heroku');
+    response.send('Goodby infrastructure');
 });
 
 app.listen(process.env.PORT || 5000);
